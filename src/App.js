3b29097bde76b988d1d7412784e5a460a3c91a26
faker007 <rlaqjadud195@naver.com>
2021-12-17 08:16:04 +0900

    [ADD] add libary:bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
+import Congrats from './Congrats';
+import GuessedWords from './GuessedWords';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <h1>Word Guess Game</h1>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
+      />
+    </div>
+  );
 }
 
 export default App;
